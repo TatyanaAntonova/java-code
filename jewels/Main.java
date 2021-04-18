f5e381cd619cc5a19b7452d81b05d0d075da6514
TatyanaAntonova <82159470+TatyanaAntonova@users.noreply.github.com>
2021-04-18 15:13:51 +0300

    Update Main.java

--- jewels/Main.java
+++ jewels/Main.java
@@ -1,8 +1,26 @@
+
 public class Main {
     public static void main(String[] args) {
         Jewels j = new Jewels();
         String jewels = "aAb";
         String stones = "caсAcA";
         System.out.println(j.numJewelsInStones(jewels, stones));
+
+        Main j2 = new Main();
+        System.out.println(j2.numJewelsInStones2(jewels, stones));
+    }
+
+    public int numJewelsInStones2(String jewels, String stones){
+        char[] jewelsArray = jewels.toCharArray();
+        char[] stonesArray = stones.toCharArray();
+
+        int count = 0;
+        for(char jewelsExamle : jewelsArray){
+            for (char stonesExample : stonesArray)
+                if(jewelsExamle == stonesExample){
+                    count++;
+                }
+        }
+        return count;
     }
 }
